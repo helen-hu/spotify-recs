db787f0372721cd473c2dcc3c3cdc2192644938b
helen-hu <helenhu.pa@gmail.com>
2021-02-05 12:27:43 -0500

    organize seeds

--- client/src/components/pages/Home.js
+++ client/src/components/pages/Home.js
@@ -21,50 +21,79 @@ class Home extends Component {
         seed_artists: ["3Nrfpe0tUJi4K4DXYWgMUX"],
         // seed_genres: [],
         // seed_tracks: [],
+      },
+      acousticness: {
         // min_acousticness: 0,
         // max_acousticness: null,
         // target_acousticness: null,
+      },
+      danceability: {
         // min_danceability: null,
         // max_danceability: null,
         // target_danceability: null,
+      },
+      duration: {
         // min_duration_ms: null,
         // max_duration_ms: null,
         // target_duration_ms: null,
-        // min_energy: 0.4,
+      },
+      energy: {
+        min_energy: 0.4,
         // max_energy: null,
         // target_energy: null,
+      },
+      instrumentalness: {
         // min_instrumentalness: null,
         // max_instrumentalness: null,
         // target_instrumentalness: null,
+      },
+      key: {
         // min_key: null,
         // max_key: null,
         // target_key: null,
+      },
+      liveness: {
         // min_liveness: null,
         // max_liveness: null,
         // target_liveness: null,
+      },
+      loudness: {
         // min_loudness: null,
         // max_loudness: null,
         // target_loudness: null,
+      },
+      mode: {
         // min_mode: null,
         // max_mode: null,
         // target_mode: null,
-        // min_popularity: 50,
-        // max_popularity: null,
+      },
+      popularity: {
+        min_popularity: 0,
+        max_popularity: 80,
         // target_popularity: null,
+      },
+      speechiness: {
         // min_speechiness: null,
         // max_speechiness: null,
         // target_speechiness: null,
+      },
+      tempo: {
         // min_tempo: null,
         // max_tempo: null,
         // target_tempo: null,
+      },
+      time_signature: {
         // min_time_signature: null,
         // max_time_signature: null,
         // target_time_signature: null,
+      },
+      valence: {
         // min_valence: null,
         // max_valence: null,
         // target_valence: null,
       },
       recs: [],
+      seedResults: [],
     };
   }
 
@@ -85,8 +114,10 @@ class Home extends Component {
       const seeds = data.seeds;
       const tracks = data.tracks;
       console.log(tracks);
+      console.log(seeds);
       this.setState({
         recs: tracks,
+        seedResults: seeds,
       });
     });
   };
@@ -117,6 +148,14 @@ class Home extends Component {
             <h3>ur tracks</h3>
             {this.state.recs.map((track) => {
               return <div key={track.id}>{track.name}</div>;
+            })}
+            <h3>ur seeds</h3>
+            {this.state.seedResults.map((seed) => {
+              return (
+                <div key={seed.id}>
+                  {seed.type}: {seed.id}
+                </div>
+              );
             })}
           </div>
         </div>
